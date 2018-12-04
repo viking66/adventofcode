2c92ae0527e5999147b2f232473de93b915dcb7b
Jason Davidson <jad658@gmail.com>
2018-12-03 18:01:34 -0800

    Add first half of solution for day 3

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,6 +1,41 @@
 module Day03 (day03) where
 
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
+
+import Parser
 import Types
+
+data Claim = Claim { idx :: Int
+                   , xInit :: Int
+                   , yInit :: Int
+                   , xLen :: Int
+                   , yLen :: Int
+                   }
+  deriving Show
+
+parseLine :: String -> Maybe Claim
+parseLine s = fst <$> runParser claimParser s
+  where idxParse = char '#' *> number <* (spaces *> char '@' *> spaces)
+        xInitXParse = number <* char ','
+        yInitParse = number <* (char ':' *> spaces)
+        xLenParse = number <* char 'x'
+        yLenParse = number
+        claimParser = Claim <$> idxParse <*> xInitXParse <*> yInitParse <*> xLenParse <*> yLenParse
+
+parseInput :: String -> Maybe [Claim]
+parseInput = traverse parseLine . lines
+
+claimIdxs :: Claim -> [(Int, Int)]
+claimIdxs (Claim _ x y xl yl) = [(a,b) | a <- [x..x+xl-1], b <- [y..y+yl-1]]
+
+claimIdxMap :: [Claim] -> Map (Int, Int) Int
+claimIdxMap = foldr f Map.empty . concatMap claimIdxs
+  where f k = Map.insertWithKey g k 1
+        g _ new old = new + old
+
+countOverlap :: [Claim] -> Int
+countOverlap = length . filter (>1) . Map.elems . claimIdxMap
+
 day03 :: String -> Showable
-day03 _ = let x = (0, 0) :: (Int, Int)
-          in pack x
+day03 = maybe (pack "Failed to parse input") (pack . countOverlap) . parseInput
---
+++ src/Parser.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Parser where
+
+import Control.Applicative hiding (many)
+import Control.Arrow (first)
+import Data.Bool (bool)
+import Data.Char (isDigit)
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f (Parser g) = Parser $ (first f <$>) . g
+
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure a = Parser $ Just . ((,) a)
+
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  Parser f <*> Parser g = Parser $ maybe Nothing (\(h, s) -> first h <$> g s) . f
+
+instance Alternative Parser where
+  empty :: Parser a
+  empty = Parser (const Nothing)
+
+  (<|>) :: Parser a -> Parser a -> Parser a
+  Parser f <|> Parser g = Parser $ liftA2 (<|>) f g
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser $ (\case
+                        [] -> Nothing
+                        (x:xs) -> bool Nothing (Just (x, xs)) (p x))
+
+char :: Char -> Parser Char
+char = satisfy . (==)
+
+many :: Parser a -> Parser [a]
+many p = many1 p <|> pure []
+
+many1 :: Parser a -> Parser [a]
+many1 p = liftA2 (:) p (many p)
+
+digit :: Parser Char
+digit = satisfy isDigit
+
+digits :: Parser [Char]
+digits = many digit
+
+number :: Parser Int
+number = read <$> digits
+
+space :: Parser Char
+space = char ' '
+
+spaces :: Parser [Char]
+spaces = many space
